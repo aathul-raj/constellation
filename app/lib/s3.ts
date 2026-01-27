0b8a13f9f7acd4ab49ca6fa0e6ed814324b58e66
Cam <cameron28202@gmail.com>
2026-01-27 01:26:12 -0600

    aws region

--- app/lib/s3.ts
+++ app/lib/s3.ts
@@ -4,7 +4,7 @@ import { Upload } from '@aws-sdk/lib-storage';
 
 // Initialize S3 Client
 const s3Client = new S3Client({
-  region: process.env.NEXT_PUBLIC_AWS_REGION || 'us-east-1',
+  region: process.env.AWS_REGION || process.env.NEXT_PUBLIC_AWS_REGION || 'us-east-2',
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID || '',
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || '',
